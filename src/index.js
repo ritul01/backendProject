d5b77c9d18d696a58bf3feb287a7f4e43c6c8502
Singh <ritulsingh123700@gmail.com>
2024-06-05 23:20:13 +0530

    error handling

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ import connectDB from "./db/index.js";
 // })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`servr is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log("mongoDB connection failed!!! ",error);
+})
 
 
 
