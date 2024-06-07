b18bab9d6ad6f5011a629cd30a6622c2c77660ab
Singh <ritulsingh123700@gmail.com>
2024-06-07 22:58:54 +0530

    some debug

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -14,14 +14,18 @@ const registerUser=asyncHandler(async(req,res)=>{
     ) {
         throw new ApiError(400, "All fields are required")
     }
-    const existingUser=User.findOne({
+    const existingUser=await User.findOne({
         $or:[{username},{email}]
     })
     if(existingUser){
         throw new ApiError(400,"user with email or username already exists")
     }
     const avatarLocalPath=req.files?.avatar[0]?.path;
-    const coverImageLocalPath=req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath=req.files?.coverImage[0]?.path;
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0){
+        coverImageLocalPath=req.files.coverImage[0].path
+    }
     if(!avatarLocalPath){
         throw new ApiError(400,"Avatar file is required")
     }
