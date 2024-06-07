5eb94ec4a2025fef2bea85f82a238a3451f8ab54
Singh <ritulsingh123700@gmail.com>
2024-06-07 17:33:13 +0530

    user register

--- src/app.js
+++ src/app.js
@@ -12,4 +12,11 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+// impoer routes
+import userRouter from "./routes/user.routes.js"
+
+// routes declaration
+app.use("/api/v1/users",userRouter)
+
 export {app}
